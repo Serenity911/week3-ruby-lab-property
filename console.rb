86514a45a4d7e091505225cd87cf1059cc814365
Serenity911 <silvia.simonassi01@gmail.com>
2019-12-03 15:14:01 +0000

    save method added

---
+++ console.rb
@@ -0,0 +1,20 @@
+require('pry')
+require_relative('model/property')
+
+property1 = Property.new({
+  'address' => "7 Castle Terrace",
+  'value' => "1000000",
+  'number_of_bedrooms' => 15 ,
+  'buy_let_status' => "let"
+  })
+
+  property2 = Property.new({
+    'address' => "17 E Market Street",
+    'value' => "800000",
+    'number_of_bedrooms' => 2 ,
+    'buy_let_status' => "on sale"
+    })
+
+
+binding.pry
+nil
---
+++ model/property.rb
@@ -0,0 +1,31 @@
+require('pg')
+
+class Property
+
+attr_reader :id, :address, :value, :number_of_bedrooms, :buy_let_status
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @address = options['address']
+    @value = options['value']
+    @number_of_bedrooms = options['number_of_bedrooms']
+    @buy_let_status = options['buy_let_status']
+  end
+
+  def save()
+    db = PG.connect({dbname: 'properties', host: 'localhost'})
+    sql = "
+    INSERT INTO properties (
+      address,
+      value,
+      number_of_bedrooms,
+      buy_let_status
+    ) VALUES ($1, $2, $3, $4) RETURNING id;
+    "
+    values = [@address, @value, @number_of_bedrooms, @buy_let_status]
+    db.prepare("save", sql)
+    @id = db.exec_prepared("save", values)[0]['id'].to_i
+    db.close()
+  end
+
+end
